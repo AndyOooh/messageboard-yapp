2c1e0a7e6082a707b9dad3aaa45b1886addc9819
Andreas Oee <andy.oeee@gmail.com>
2025-03-15 17:39:22 +0700

    fix: ts issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,16 @@ export default async function Home() {
   console.log("Fetching posts...");
   const initialPosts = await getAll({ limit: 10 });
 
-  console.log("🚀 initialPosts length:", initialPosts.length);
+  console.log("🚀 initialPosts length:", initialPosts?.length);
   console.log("🚀 initialPosts:", initialPosts);
+
   return (
     <main>
       <Container size='3' py='6'>
         <Heading size='8' mb='6'>
           Message Board
         </Heading>
-        <PostList initialPosts={initialPosts} />
+        <PostList initialPosts={initialPosts || []} />
       </Container>
     </main>
   );
